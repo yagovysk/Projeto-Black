f491f31479e0c0b15ca49b20d441b6bacb0663e5
Yago Cerqueira Regis <45441676+yagovysk@users.noreply.github.com>
2024-02-05 17:07:14 -0300

    Update Menu.jsx

--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -51,7 +51,7 @@ export function Menu() {
           aria-label="item clicável, rolagem para a página Home."
           className="item-list"
           onClick={() => scrollToSection('home')}>
-          Home
+          Início
         </li>
         <li
           aria-label="item clicável, rolagem para a página Sobre Mim."
