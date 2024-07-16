44fd7242ec96545bc87968029aa2ce7a176f8532
yagovysk <yago09regis@gmail.com>
2024-07-16 10:06:28 -0300

    add

--- src/Components/About/Sobre.jsx
+++ src/Components/About/Sobre.jsx
@@ -1,8 +1,8 @@
-import img from '../../assets/img-about.jpeg';
-import { FaSquareWhatsapp } from 'react-icons/fa6';
-import { FaInstagramSquare } from 'react-icons/fa';
-import VisibilitySensor from 'react-visibility-sensor';
-import './Sobre.css';
+import img from "../../assets/img-about.jpeg";
+import { FaSquareWhatsapp } from "react-icons/fa6";
+import { FaInstagramSquare } from "react-icons/fa";
+import VisibilitySensor from "react-visibility-sensor";
+import "./Sobre.css";
 
 export function Sobre() {
   return (
@@ -11,25 +11,29 @@ export function Sobre() {
         <VisibilitySensor partialVisibility>
           {({ isVisible }) => (
             <article
-              className={`container-card ${isVisible ? 'animated' : ''}`}>
+              className={`container-card ${isVisible ? "animated" : ""}`}
+            >
               <h2>Um pouco sobre mim</h2>
               <p>
                 Meu nome é Antônio Sousa, tenho 37 anos, conhecido como
-                Treinador Sousa Black. Pós-Graduado em Gerontologia, foco em
-                treinos adaptados para idosos, obesidade, ganho de massa, perda
-                de peso. Leciono Aula de Boxe, para iniciantes e competidores.
-                Busco promover bem-estar e superação em cada treino.
-                Comprometido com a saúde e condicionamento dos meus alunos.
+                Treinador Sousa Black. Bacharelado em educação física,
+                Pós-Graduado em Gerontologia, foco em treinos adaptados para
+                idosos, obesidade, ganho de massa, perda de peso. Leciono Aula
+                de Boxe, para iniciantes e competidores. Busco promover
+                bem-estar e superação em cada treino. Comprometido com a saúde e
+                condicionamento dos meus alunos.
               </p>
               <div className="container-icon">
                 <a
                   href="https://wa.me/556199725414?text=Olá, vim pelo site, gostaria de saber os planos Sousa Black."
-                  target="_blank">
+                  target="_blank"
+                >
                   <FaSquareWhatsapp className="card-icon" />
                 </a>
                 <a
                   href="https://www.instagram.com/sousablackoficial/"
-                  target="_blank">
+                  target="_blank"
+                >
                   <FaInstagramSquare className="card-icon" />
                 </a>
               </div>
@@ -44,7 +48,7 @@ export function Sobre() {
               id="sobre-mim"
               src={img}
               alt=""
-              className={isVisible ? 'animated' : ''}
+              className={isVisible ? "animated" : ""}
             />
           )}
         </VisibilitySensor>
